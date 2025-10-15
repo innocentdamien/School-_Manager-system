8b848c5e0485867df7cd7822cf7d88963de8fe25
innocentdamien <sylverrahdamien@gmail.com>
2025-10-15 15:59:37 +0300

    vercel configuration

--- caseflow-forms-hub-main/src/pages/Index.tsx
+++ caseflow-forms-hub-main/src/pages/Index.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { Button } from "@/components/ui/button";
+import { Button } from "../components/ui/button";
 import { GraduationCap, Users, Shield } from "lucide-react";
 
 const Index = () => {
